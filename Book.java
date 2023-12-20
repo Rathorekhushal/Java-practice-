c5e40a05b1ddb6d0592bf64afc1518b9fe84354b
Rathorekhushal <110766141+Rathorekhushal@users.noreply.github.com>
2023-12-20 04:55:51 +0000

    uploaded
    Here is the library management System project

--- Book.java
+++ Book.java
@@ -1,164 +1,126 @@
+package Smallproject;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-class Book {
-    private String isbn;
-    private String title;
-    private String author;
-    private boolean available;
+public class Book {
+    private String ISbN;
+    private String Tittle;
+    private String Author;
+    private boolean Avialable;
+    public Book(String ISbN, String Tittle,String Author,boolean Avialable){
+        this.ISbN=ISbN;
+        this.Tittle=Tittle;
+        this.Author=Author;
+        this.Avialable=Avialable;
 
-    public Book(String isbn, String title, String author, boolean available) {
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.available = available;
+
     }
-
-    public String getIsbn() {
-        return isbn;
+    public String getISbN(){
+        return ISbN;
     }
+    public String getTittle(){
+        return Tittle;
 
-    public String getTitle() {
-        return title;
     }
-
-    public String getAuthor() {
-        return author;
+    public String getAuthor(){
+        return Author;
     }
-
-    public boolean isAvailable() {
-        return available;
+    public  boolean isAvailable(){
+        return Avialable;
     }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
+    public void setAvailable(boolean Available){
+        this.Avialable=Avialable;
     }
-
-    // public String toString() {
-    //     return "Book{" +
-    //             "isbn='" + isbn + '\'' +
-    //             ", title='" + title + '\'' +
-    //             ", author='" + author + '\'' +
-    //             ", available=" + available +
-    //             '}';
-    // }
-}
-
-class Library {
-    private String name;
-    private List<Book> books;
-
-    public Library(String name) {
-        this.name = name;
-        this.books = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void addBook(Book book) {
-        books.add(book);
-    }
-
-    public void removeBook(Book book) {
-        books.remove(book);
-    }
-
-    public void displayBooks() {
-        System.out.println("Books in " + name + " Library:");
-        for (Book book : books) {
-            System.out.println(book);
+    public void CheckOut(){
+        if(Avialable==false){
+            System.out.println("Book checked out"+Tittle);
+        }else{
+            System.out.println("Book not available "+Tittle);
         }
-        System.out.println();
+    }
+    public void CheckIn(){
+        if(Avialable==true){
+            System.out.println("Book checked in"+Tittle);
+        }
+    }
+    // Properties: User ID, name, contact information, books borrowed, etc.
+    // Methods: Getters and setters, methods to display user information.
+}
+class User{
+    private int UserID;
+    private String Name;
+    private String ContactINFO;
+    private List<Book> borrowedBooks;
+    public User(int UserID, String Name, String ContactINFO){
+        this.UserID=UserID;
+        this.ContactINFO=ContactINFO;
+        this.Name=Name;
+        this.borrowedBooks=new ArrayList<>();
+    }
+   public String getName(){
+    return Name;
+   }
+    public int getUserID(){
+        return UserID;
+    }
+    public String getContactINFO(){
+        return ContactINFO;
+    }
+    public void borrowedBook(Book book) {
+        if (book.isAvailable()) {
+            borrowedBooks.add(book);
+            book.CheckOut();
+            System.out.println(Name + " borrowed: " + book.getTittle());
+        } else {
+            System.out.println("Book not available for borrowing: " + book.getTittle());
+        }
+    }
+    public void returnBooks(Book book){
+        if(borrowedBooks.contains(book)){
+            borrowedBooks.remove(book);
+            book.CheckIn();
+            System.out.println("rutined "+book.getTittle());
+        }
+        else{
+            System.out.println("Did not borrow"+book.getTittle());
+        }
+
     }
 }
-
-class User {
-    private int userId;
-    private String name;
-    private List<Book> borrowedBooks;
-
-    public User(int userId, String name) {
-        this.userId = userId;
-        this.name = name;
-        this.borrowedBooks = new ArrayList<>();
+class Library{
+    private List<Book> books;
+    private List<User> users;
+    // private List<Transaction> transactions;
+    public Library(){
+        this.books=new ArrayList<>();
+        this.users=new ArrayList<>();
     }
+}
+ class Transaction {
+    private int transactionId;
+    private int bookId;
+    private int userId;
+    public Transaction(int transactionId,int bookId,int userId){
+        this.bookId=bookId;
+        this.transactionId=transactionId;
+        this.userId=userId;
+    }
+    public int getbookId(){
+        return bookId;
 
-    public int getUserId() {
+    }
+    public int getUserId(){
         return userId;
     }
-
-    public String getName() {
-        return name;
+    public int gettransctionId(){
+        return transactionId;
     }
 
-    public List<Book> getBorrowedBooks() {
-        return borrowedBooks;
-    }
 
-    public void borrowBook(Library library, String isbn) {
-        for (Book book : library.getBooks()) {
-            if (book.getIsbn().equals(isbn) && book.isAvailable()) {
-                book.setAvailable(false);
-                borrowedBooks.add(book);
-                System.out.println(name + " has borrowed: " + book.getTitle());
-                return;
-            }
-        }
-        System.out.println("Book with ISBN " + isbn + " not available for borrowing.");
-    }
-
-    public void returnBook(Library library, String isbn) {
-        for (Book book : borrowedBooks) {
-            if (book.getIsbn().equals(isbn)) {
-                book.setAvailable(true);
-                borrowedBooks.remove(book);
-                System.out.println(name + " has returned: " + book.getTitle());
-                return;
-            }
-        }
-        System.out.println("Book with ISBN " + isbn + " not found in the list of borrowed books.");
-    }
-
-    public void displayBorrowedBooks() {
-        System.out.println(name + "'s Borrowed Books:");
-        for (Book book : borrowedBooks) {
-            System.out.println(book);
-        }
-        System.out.println();
-    }
+public class LibraryManagment{
+    Book book1 =new Book("322", "java programming", "jhon", true);
+    Book book2 = new Book("456", "Object-Oriented-Design", "Jane-Smith",true);
+    User user1 = new User(1, "khushal", "alice@example.com");
+    User user2 = new User(2, "rathore", "bob@example.com");
 }
-
-public class Library {
-    public static void main(String[] args) {
-        // Instantiate Library and User objects
-        Library library = new Library("MyLibrary");
-        User user = new User(1, "khushal");
-
-        // Add some books to the library
-        library.addBook(new Book("123456789", "Java Programming", "jhon smith", true));
-        library.addBook(new Book("987654321", "Python Basics", "Jane Doe", true));
-
-        // Display available books in the library
-        library.displayBooks();
-
-        // Allow user to borrow a book
-        user.borrowBook(library, "123456789");
-
-        // Display user's borrowed books
-        user.displayBorrowedBooks();
-
-        // Allow user to return a book
-        user.returnBook(library, "123456789");
-
-        // Display available books in the library after returning the book
-        library.displayBooks();
-    }
 }
-// one error in main method 
